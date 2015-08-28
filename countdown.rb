ed5bab90fe70b38a60d066685021c74473cf98c7
mirc89 <kmircovich1@gmail.com>
2015-08-27 22:46:31 -0400

    happy new year

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,23 @@
-def countdown
-  #write your code here
+def countdown(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  #puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
+
+#countdown(5)
+
+
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    sleep(1)
+    int -= 1
+  end
+  #puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
+end
+
+#countdown_with_sleep(5)
